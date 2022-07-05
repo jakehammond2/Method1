6b52e8a35c0b27c4b69c5af54f7884995c81d076
Jake Hammond <jakehammond226@gmail.com>
2022-07-05 10:57:59 -0500

    method1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("What is your age?");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What is your favorite animal?");
+string animal = Console.ReadLine();
+
+Console.WriteLine($"My name is {name} and I am {num} years old. My favorite animal is a {animal}.");
